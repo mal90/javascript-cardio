a6267ef710e58941176918946407590ea20d8b6a
Malik <madkonline@gmail.com>
2019-12-29 13:19:29 +0800

    Add comments for understanding

--- quicksort.js
+++ quicksort.js
@@ -16,3 +16,18 @@ function quickSort(arr) {
 }
 
 console.log(quickSort([2, 4, 1, 3, 9, 4, 5, 6, 0, 2]));
+
+/**
+ * pivot is the 0th element of the array
+ * add rest of the elements on to another array named rest
+ *
+ * left array is the elements less or equal than pivot and right is the opposite
+ *
+ * for example first round
+ *
+ * pivot = 2
+ * left = 1, 0
+ * right = 4, 3, 9, 4, 5, 6, 2
+ *
+ * then do this recursively and concat the pivot in the middle and concat right after that
+ */
